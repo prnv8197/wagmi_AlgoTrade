d98aca231303fe84e74faef64a6a76b5172c2be9
prampey <prudhvi.rampey@gmail.com>
2022-05-20 17:23:53 +0530

    Order management
    - Pending orders are checked before a new trade is placed
    - Orders are managed carefully when a trade is in place
    - Orders are cleaned up after the trade

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import time
 from dydx3 import Client
 import dydx3.constants as consts
 from dydx3 import private_key_to_public_key_pair_hex
+from sympy import false, true
 from web3 import Web3
 import os
 from dotenv import load_dotenv
@@ -59,12 +60,12 @@ def go_long(dydx_client, amount, stop_loss, roi):
         'expiration_epoch_seconds': time.time() + 15000,
         'time_in_force': consts.TIME_IN_FORCE_IOC
     }
-    order_response = dydx_client.private.create_order(**order_params)
+    market_buy_order = dydx_client.private.create_order(**order_params).data
     # TODO: Might need to check here if order filled or not!
-    print(order_response.data)
+    print(market_buy_order)
 
-    buy_price = order_response.data['order']['price']
-   
+    buy_price = market_buy_order['order']['price']
+
     # Also make a stop loss order
     stoploss_order = client.private.create_order(
         position_id=position_id,
@@ -73,12 +74,12 @@ def go_long(dydx_client, amount, stop_loss, roi):
         order_type=consts.ORDER_TYPE_TRAILING_STOP,
         post_only=False,
         size=str(amount),
-        price="1", # Dunno what to do here
-        trailing_percent='-%f' % stop_loss, #NEed to switch back to triggerPrice
+        price="1",  # Dunno what to do here
+        trailing_percent='-%f' % stop_loss,  # NEed to switch back to triggerPrice
         limit_fee='0.015',
         expiration_epoch_seconds=time.time() + 15000,
-    )
-    print(stoploss_order.data)
+    ).data
+    print(stoploss_order)
 
     take_profit_price = '%.1f' % (float(buy_price) * (1 + (roi/100)))
     trigger_profit_price = '%.1f' % (float(buy_price) * (1 + (roi/200)))
@@ -94,18 +95,87 @@ def go_long(dydx_client, amount, stop_loss, roi):
         trigger_price=trigger_profit_price,
         limit_fee='0.015',
         expiration_epoch_seconds=time.time() + 15000,
-    )
-    print(take_profit_order.data)
+    ).data
+    print(take_profit_order)
+
+    return {
+        "market_buy_order": market_buy_order,
+        "stop_loss_order": stoploss_order,
+        "take_profit_order": take_profit_order
+    }
 
 
-def check_active_orders(dydx_client):
-    # Count open orders (there should be exactly one).
-    orders_response = dydx_client.private.get_active_orders(
-        market=consts.MARKET_ETH_USD,
-        # status=consts.ORDER_STATUS_OPEN,
-    )
-    print(orders_response.data)
-    return len(orders_response.data['orders']) > 0
+def check_if_pending(orders, dydx_client):
+    # GEt current active orders
+    # If none, return false
+    # Else check for a buy order
+    # if there, check status
+    if orders == {}:
+        print("No active orders!")
+        return False
+    # MAke sure the market buy order is not cancelled
+    market_buy_order = dydx_client.private.get_order_by_id(
+        orders['market_buy_order']['order']['id']).data
+    # Else, clear out orders
+    order_status = market_buy_order['order']['status']
+
+    # If pending, return true
+    if (order_status == consts.ORDER_STATUS_PENDING):
+        print("buy order pending!")
+        return True
+    # If filled, check stop orders
+    elif (order_status == consts.ORDER_STATUS_FILLED):
+        print("buy order filled!")
+        # Check if we've taken stop loss or profit
+        stop_loss_order = dydx_client.private.get_order_by_id(
+            orders['stop_loss_order']['order']['id']).data
+        if stop_loss_order['order']['status'] == consts.ORDER_STATUS_FILLED:
+            print("Target reached! Ready for new orders")
+            # if we have, cancel the other order and return false
+            dydx_client.private.cancel_order(
+                order_id=orders['take_profit_order']['order']['id'])
+            orders = {}
+            return false
+
+        take_profit_order = dydx_client.private.get_order_by_id(
+            orders['take_profit_order']['order']['id']).data
+        if take_profit_order['order']['status'] == consts.ORDER_STATUS_FILLED:
+            print("Target reached! Ready for new orders")
+            # if we have, cancel the other order and return false
+            dydx_client.private.cancel_order(
+                order_id=orders['stop_loss_order']['order']['id'])
+            orders = {}
+            return false
+
+        if stop_loss_order['order']['status'] == consts.ORDER_STATUS_CANCELED or take_profit_order['order']['status'] == consts.ORDER_STATUS_CANCELED:
+            print("One of the target orders canceled Ready for new orders")
+            # clear out remaining sell orders
+            try:
+                dydx_client.private.cancel_order(
+                    order_id=orders['stop_loss_order']['order']['id'])
+                dydx_client.private.cancel_order(
+                    order_id=orders['take_profit_order']['order']['id'])
+                # clear out the long position too
+                dydx_client.private.cancel_order(
+                    order_id=orders['market_buy_order']['order']['id'])
+            except:
+                print("One order was already canceled")
+            # Reset current_orders
+            orders = {}
+            return False
+        # IF we haven't, keep waiting return true
+        print("Waiting on profit/loss target")
+        return True
+    else:
+        print("Buy order not fulfilled. Ready for new orders!")
+        # clear out remaining sell orders
+        dydx_client.private.cancel_order(
+            order_id=orders['stop_loss_order']['order']['id'])
+        dydx_client.private.cancel_order(
+            order_id=orders['take_profit_order']['order']['id'])
+        # Reset current_orders
+        orders = {}
+        return False
 
 # def go_short(dydx_client):
 #     # Get our position ID.
@@ -127,23 +197,23 @@ def check_active_orders(dydx_client):
 
 
 def start_bot(dydx_client):
+    current_orders = {}
     while(True):
         # Run the strat every x seconds
         time.sleep(process_throttle_secs)
 
-        # Check for open orders and skip this loop if there is one
-        if check_active_orders(dydx_client):
-            print("You have active orders!")
+        # If there pending open orders, no trades will be placed
+        if check_if_pending(current_orders, dydx_client):
             continue
+        
+        # Run our strategy
         if (MyStrategy1() == "Long"):
-            print("Going long")
-            go_long(dydx_client, amount=0.01, stop_loss=1, roi=1)
+            current_orders = go_long(
+                dydx_client, amount=0.01, stop_loss=1, roi=1)
         elif(MyStrategy1() == "Short"):
             print("short!")
-        else:
-            print("Pass")
 
 
 if __name__ == "__main__":
     client = setup_dydx()
-    start_bot(client) 
+    start_bot(client)
